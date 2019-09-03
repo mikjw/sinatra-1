37236391a711112baa515f94ebca2b111eb797c6
Thomas Ross <thomasross3610@gmail.com>
2019-09-03 17:14:59 +0100

    player 1 and 2 name inputs added

--- app.rb
+++ app.rb
@@ -11,6 +11,7 @@ class Battle < Sinatra::Base
 
   post '/names' do
     @player_1_name = params[:player_1_name]
+    @player_2_name = params[:player_2_name]
     erb(:play)
   end
 
--- spec/features/testing_name_input_spec.rb
+++ spec/features/testing_name_input_spec.rb
@@ -2,11 +2,8 @@ feature 'Testing name input' do
   scenario 'User can enter name in form' do
     visit('/')
     fill_in('player_1_name', with: 'Tom')
+    fill_in('player_2_name', with: 'Mike')
     click_button('Submit')
-    expect(page).to have_content 'Welcome, Tom'
+    expect(page).to have_content 'Welcome, Tom and Mike'
   end
 end
-
-
-
-
